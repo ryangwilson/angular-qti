ba8e1551e9c8d7a459dfca79f31e7ec35beff6a9
Rob Taylor <roboncode@gmail.com>
2014-12-01 14:45:48 -0700

    added missing files

--- src/elements/assessment/assessment.js
+++ src/elements/assessment/assessment.js
@@ -4,6 +4,7 @@ angular.module('qti').directive('assessment', function () {
         restrict: 'E',
         scope: true,
         transclude: true,
+        replace: true,
         templateUrl: 'templates/assessment.html',
         link: function (scope, el, attr) {
             scope.assessment = {
