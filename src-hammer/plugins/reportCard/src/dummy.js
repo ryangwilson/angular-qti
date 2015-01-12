46da3691b2a25b613972146da9936b80e4a2bfd7
Rob Taylor <roboncode@gmail.com>
2015-01-12 08:50:19 -0700

    removed src-hammer

--- src-hammer/plugins/reportCard/src/dummy.js
+++
@@ -1,9 +0,0 @@
-/**
- * This is a dependency of reportCard. Files can be broken out to separate tasks internally
- */
-internal('reportCard.dummy', ['framework'], function (framework) {
-    framework.fire('dummy::init');
-    return function () {
-        return 'dummy';
-    };
-});
--- src-hammer/plugins/reportCard/src/reportCard.js
+++
@@ -1,9 +0,0 @@
-/* global define, internal */
-/**
- * reportCard.dummy is a dependency of reportcard
- */
-internal('reportCard', ['framework', 'reportCard.dummy'], function (framework, dummy) {
-    framework.fire('reportCard::init');
-    console.log('%c[reportCard] calling dummy()', 'color: #999', dummy());
-});
-
